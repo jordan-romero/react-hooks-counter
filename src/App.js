977147492747dcd4941dddf2d167d2d4b873fd37
jordles113 <jordan.t.romero@gmail.com>
2021-01-27 14:38:20 -0600

    writing calculate function

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@
 import './App.css';
 
 function App() {
+
+  const calculateTimeLeft = () => {
+
+    let year = new Date().getFullYear();
+    
+    const difference = +new Date(`10/20/${year}`) -new Date()
+
+    let timeLeft = {}
+
+    
+    }
   return (
     <>
-      
+    
     </>
   );
 }
