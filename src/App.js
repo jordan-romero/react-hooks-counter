38aa1063b6498128a4acdcf0f3dff993daf4a214
jordles113 <jordan.t.romero@gmail.com>
2021-01-27 14:43:33 -0600

    builds out use effect hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
 
   const calculateTimeLeft = () => {
 
@@ -11,8 +11,27 @@ function App() {
 
     let timeLeft = {}
 
-    
+      if (difference > 0) {
+        timeLeft = {
+          days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+          hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+          minutes: Math.floor((difference / 1000 / 60) % 60),
+          seconds: Math.floor((difference / 1000) % 60)
+        };
+      }
+    return timeLeft
     }
+
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+    useEffect(() => {
+      const timer = setTimeout(() => {
+        setTimeLeft(calculateTimeLeft())
+      }, 1000)
+      return () => clearTimeout(timer);
+    })
+
+    const timerComponents = []
   return (
     <>
     
